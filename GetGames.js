f8ecabac7185ea9aadff78907ba5911082ddb01d
Tucker <tmiles7@vols.utk.edu>
2020-01-05 10:31:09 -0500

    successfully outputting owned games to console

--- GetGames.js
+++ GetGames.js
@@ -10,9 +10,18 @@ fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122
   });
   */
 
+  //returns length
+  function length(obj) {
+    return Object.keys(obj).length;
+  }
+
+
  function doSomething(success){
     //do whatever you like
-    console.log(success);
+    var response = JSON.parse(JSON.stringify(success));
+    for (var i = 0; i < length(response.response.games); i++) {
+      console.log(response.response.games[i].name);  //Logging proper syntax to just retrieve games.
+    }
   }
  
   fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
