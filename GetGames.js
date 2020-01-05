c37e5b04e4f4d2cf7f0bf39dc717120e2fc3be7d
Tucker <tmiles7@vols.utk.edu>
2020-01-04 23:15:55 -0500

    restructured request

--- GetGames.js
+++ GetGames.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch"); //To make request
 
+/*
 fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
   .then((response) => {
     return response.json();
@@ -7,3 +8,13 @@ fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122
   .then((myJson) => {
     console.log(myJson);
   });
+  */
+
+ function doSomething(success){
+    //do whatever you like
+    console.log(success);
+  }
+ 
+  fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
+     .then(data => data.json())
+     .then(success => doSomething(success));
