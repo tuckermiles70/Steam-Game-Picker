a1553265b051dc56dbba9a6c7ffa83d295b1428a
Tucker <tmiles7@vols.utk.edu>
2020-01-04 23:10:45 -0500

    successfully making request to steam for games

---
+++ GetGames.js
@@ -0,0 +1,9 @@
+const fetch = require("node-fetch"); //To make request
+
+fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
+  });
