5d4946277fee60c6c30c71e7cb6f6e7afe037134
tuckermiles70 <tmiles7@vols.utk.edu>
2020-01-06 16:08:08 -0500

    successfully outputting games from array

--- Game Picker/GetGames.js
+++ Game Picker/GetGames.js
@@ -1,44 +1,3 @@
-//const fetch = require("node-fetch"); //To make request
-
-/*
-fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.log(myJson);
-  });
-  */
-
-  //returns length
-  function length(obj) {
-    return Object.keys(obj).length;
-  }
-
-/*
- function doSomething(success){
-    //do whatever you like
-    var response = JSON.parse(JSON.stringify(success));
-    for (var i = 0; i < length(response.response.games); i++) {
-      console.log(response.response.games[i].name);  //Logging proper syntax to just retrieve games.
-    }
-  }
- 
-  fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1')
-     .then(data => data.json())
-     .then(success => doSomething(success));
-     */
-
-     /*
-    function doSomething(success){
-      //do whatever you like
-      var response = JSON.parse(JSON.stringify(success));
-      for (var i = 0; i < length(response.response.games); i++) {
-        console.log(response.response.games[i].name);  //Logging proper syntax to just retrieve games.
-      }
-    }
-   */
-
   //returns length
   function length(obj) {
     return Object.keys(obj).length;
@@ -48,23 +7,24 @@ fetch('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122
   const Url = 'https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F778122CA758FA979CAFF41B8081EF58&steamid=76561198138426605&include_appinfo=1';
   var games = new Array();  
 
+
   $.getJSON(Url, function(data) {
     var response = JSON.parse(JSON.stringify(data));
     //var len = length(response.response.games);
     //console.log(len);
     for (var i = 0; i < length(response.response.games); i++) {
-      //games.push(response.response.games[i].name);
       var cur = response.response.games[i].name;
-      console.log('pushing ' + cur + ' onto games');
-      //games.push(cur);
+
+      //games.push(response.response.games[i].name);
+      games.push(cur);
+      
+      //console.log(games.length);
+
+      //console.log(cur);
+      
       //console.log(cur);  //Logging proper syntax to just retrieve games.
     }
+    for (var i = 0; i < games.length; i++) {
+      console.log(games[i]);
+    }
   });
-
-/*
-  console.log('botto');
-  console.log(games.length);
-  for (var i = 0; i < games.length; i++) {
-    console.log(games[i]);
-  }
-*/
